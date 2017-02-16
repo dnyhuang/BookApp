977dff12bf77f1dd9815180c659e4eb006887082
danny <danny.huang@iisigroup.com>
2017-02-16 23:41:00 +0800

    record

--- app/src/main/java/main/bookapp/MainActivity.java
+++ app/src/main/java/main/bookapp/MainActivity.java
@@ -1,15 +1,25 @@
 package main.bookapp;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import main.bookapp.data.BookHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BookHelper helper;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,41 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //Create DB
+        helper = new BookHelper(this);
+
+        //
+//        sqlLiteInsert();
+
+        //
+        ListView listview = (ListView) findViewById(R.id.listview);
+        cursor = helper.getReadableDatabase().query("books", null, null, null, null, null, null);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_expandable_list_item_2,
+                cursor,
+                new String[]{"cdate","info"},
+                new int[]{android.R.id.text1,android.R.id.text2}
+        ,0);
+        listview.setAdapter(adapter);
+
+
+
+    }
+
+    private void sqlLiteInsert() {
+        ContentValues contentValue = new ContentValues(3);
+        contentValue.put("cdate","2016-11-08");
+        contentValue.put("info","Android Books");
+        contentValue.put("amount","2000");
+
+        long id ;
+        id = helper.getWritableDatabase().insert("books", null, contentValue);
+        if (id > 0){
+            Log.i("danny","yessssss!" + id);
+        }else{
+            Log.i("danny","nooooooo!");
+        }
     }
 
     @Override
--- app/src/main/java/main/bookapp/data/BookHelper.java
+++ app/src/main/java/main/bookapp/data/BookHelper.java
@@ -1,8 +1,21 @@
 package main.bookapp.data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import main.bookapp.R;
 
 /**
  * Created by danny on 2017/2/14.
@@ -12,6 +25,9 @@ public class BookHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "books.db";
     private static final int DB_VERSION = 1;
+    private static final String CDATE = "cdate";
+    private static final String INFO = "info";
+    private static final String AMOUNT = "amount";
     private final Context context;
 
     public BookHelper(Context context){
@@ -26,7 +42,57 @@ public class BookHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        Log.i("danny","aaaaa");
+        sqLiteDatabase.execSQL("CREATE TABLE books " +
+        "(_id INTEGER PRIMARY KEY, " +
+        "cdate DATETIME NOT NULL, " +
+        "info VARCHAR ," +
+        "amount INTEGER)");
 
+
+
+        InputStream is = context.getResources().openRawResource(R.raw.book);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            String line = bufferedReader.readLine();
+            while (line != null){
+                sb.append(line);
+                sb.append("\n");
+                line = bufferedReader.readLine();
+            }
+
+            JSONObject jsonObject = new JSONObject(sb.toString());
+            JSONArray books = jsonObject.getJSONArray("books");
+
+            for (int i = 0; i < books.length(); i++) {
+                JSONObject innerObj = books.getJSONObject(i);
+                String cdate = innerObj.getString(CDATE);
+                String info = innerObj.getString(INFO);
+                int amount = innerObj.getInt(AMOUNT);
+
+                ContentValues val = new ContentValues(3);
+                val.put(CDATE,cdate);
+                val.put(INFO,info);
+                val.put(AMOUNT,amount);
+
+                long id = sqLiteDatabase.insert("books",null,val);
+
+                if(id > 0){
+                    Log.i("danny","Success");
+                }else{
+                    Log.i("danny","Fail");
+
+                }
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
